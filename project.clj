b71221ddb77b9329364d397c0110f867694987a6
James Henderson <james@jarohen.me.uk>
2014-04-21 16:11:36 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,27 @@
+(defproject jarohen/flow "0.1.0-SNAPSHOT"
+  :description "Lightweight library to help you write dynamic CLJS webapps"
+
+  :url "https://github.com/james-henderson/flow"
+
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
+
+  :plugins [[com.keminglabs/cljx "0.3.2"]]
+
+  :source-paths ["src" "target/generated/clj"]
+  
+  :hooks [cljx.hooks]
+
+  :filespecs [{:type :path
+               :path "target/generated/cljs"}]
+
+  :cljx {:builds [{:source-paths ["src"]
+                   :output-path "target/generated/clj"
+                   :rules :clj}
+
+                  {:source-paths ["src"]
+                   :output-path "target/generated/cljs"
+                   :rules :cljs}]})
