9e6dcdb33a1a4718167e3144042873e60517688d
James Henderson <james@jarohen.me.uk>
2014-06-30 22:52:30 +0100

    Adding a couple more helper fns in flow.dom

--- src/flow/dom.cljs
+++ src/flow/dom.cljs
@@ -4,3 +4,17 @@
   (.. $el
       -classList
       (add class-name)))
+
+(defn remove-class! [$el class-name]
+  (.. $el
+      -classList
+      (remove class-name)))
+
+(defn set-style! [$el k v]
+  (aset (.-style $el) (name k) (cond-> v
+                                 (keyword? v) name)))
+
+(defn set-attr! [$el k v]
+  (if-not (nil? v)
+    (.setAttribute $el (name k) v)
+    (.removeAttribute $el (name k) v)))
