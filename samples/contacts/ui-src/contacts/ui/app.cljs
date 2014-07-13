554d4a2b1d06b68068ae443397624a3697d01a44
James Henderson <james@jarohen.me.uk>
2014-07-13 19:43:57 +0100

    [contacts] Bit of a play with the contacts app

--- samples/contacts/ui-src/contacts/ui/app.cljs
+++ samples/contacts/ui-src/contacts/ui/app.cljs
@@ -1,5 +1,6 @@
 (ns contacts.ui.app
   (:require [clojure.string :as s]
+            [contacts.formatter :as cf]
             [flow.core :as f :include-macros true]
             simple-brepl.client))
 
@@ -7,8 +8,19 @@
 
 (set! (.-onload js/window)
       (fn []
-        (f/root js/document.body
-          (f/el
-            [:h1 "Hello world!"]))))
-
-
+        (let [!contacts (atom
+                         [{:first "Kate", :last "Reed"}
+                          {:first "James", :last "Henderson"}])]
+          
+          (f/root js/document.body
+            (f/el
+              [:div.container
+               [:h1 "My address book"]
+               [:ul
+                (for [contact (<<! !contacts)]
+                  [:li
+                   (cf/display-name contact)
+                   #_[:button.btn.btn-danger {::f/on {:click (let [!contact (!>> contact)]
+                                                               #(println "deleting" (pr-str @!contact)))}
+                                              ::f/style {:margin "0.2em 1em"}}
+                      "X"]])]])))))
