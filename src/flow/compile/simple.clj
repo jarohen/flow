c0a47d1d163b78b3de9a3f7b543ce3c064c5d63d
James Henderson <james@jarohen.me.uk>
2014-07-30 22:49:25 +0100

    Finally written up map & coll

--- src/flow/compile/simple.clj
+++ src/flow/compile/simple.clj
@@ -21,18 +21,51 @@
 
 ;; MAP
 
-#_(defmethod compile-el :map [{m :map} opts]
-    (let [flattened-map (flatten (seq m))
-          compiled-elems (map #(compile-el % opts) flattened-map)]
-      {:deps (set (mapcat :deps compiled-elems))
-       :inline-value (->> compiled-elems
-                          (map :inline-value)
-                          (partition 2)
-                          (into {}))}))
+(defmethod compile-value :map [{m :map} opts]
+  (let [flattened-map (flatten (seq m))
+        compiled-values (map #(compile-value % opts) flattened-map)]
+    (reify fp/CompiledValue
+      (value-deps [_]
+        (set (mapcat fp/value-deps compiled-values)))
+
+      (inline-value-form [_ state-sym]
+        (->> compiled-values
+             (map #(fp/inline-value-form % state-sym))
+             (partition 2)
+             (map #(apply vector %))
+             (into {}))))))
+
+(defmethod compile-el :map [form opts]
+  (let [compiled-map (compile-value form opts)]
+    (reify fp/CompiledElement
+      (elem-deps [_] (fp/value-deps compiled-map))
+      (bindings [_] nil)
+
+      (initial-el-form [_ state-sym]
+        (fp/inline-value-form compiled-map state-sym))
+
+      (updated-el-form [_ new-state-sym updated-vars-sym]
+        (fp/inline-value-form compiled-map new-state-sym)))))
 
 ;; COLL
 
-#_(defmethod compile-el :coll [{:keys [coll]} opts]
-    (let [compiled-elems (map #(compile-el % opts) coll)]
-      {:inline-value `(into ~(empty coll) [~@(map :inline-value compiled-elems)])
-       :deps (set (mapcat :deps compiled-elems))}))
+(defmethod compile-value :coll [{:keys [coll]} opts]
+  (let [compiled-values (map #(compile-value % opts) coll)]
+    (reify fp/CompiledValue
+      (value-deps [_]
+        (set (mapcat fp/value-deps compiled-values)))
+
+      (inline-value-form [_ state-sym]
+        `(into ~(empty coll) [~@(map #(fp/inline-value-form % state-sym) compiled-values)])))))
+
+(defmethod compile-el :coll [form opts]
+  (let [compiled-coll (compile-value form opts)]
+    (reify fp/CompiledElement
+      (elem-deps [_] (fp/value-deps compiled-coll))
+      (bindings [_] nil)
+
+      (initial-el-form [_ state-sym]
+        (fp/inline-value-form compiled-coll state-sym))
+
+      (updated-el-form [_ new-state-sym updated-vars-sym]
+        (fp/inline-value-form compiled-coll new-state-sym)))))
