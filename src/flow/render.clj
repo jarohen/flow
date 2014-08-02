9d3417b82c6e3d42aa672f1c208d933d07363c38
James Henderson <james@jarohen.me.uk>
2014-08-02 17:15:00 +0100

    Splitting render out into some CLJS bits

--- src/flow/render.clj
+++ src/flow/render.clj
@@ -4,43 +4,10 @@
 
 (alias 'fd (doto 'flow.dom create-ns))
 
-(defn render-el [compiled-elem path]
-  (let [!state (u/path->sym "!" path "state")
-        notify! (u/path->sym path "notify!")
-        handler (u/path->sym path "handler")
-
-        !el (u/path->sym "!" path "el")
-        
-        state (u/path->sym path "state")
-        new-state (u/path->sym path "new-state")
-        updated-vars (u/path->sym path "updated-vars")
-
-        deps (fp/identity-deps compiled-elem)]
-
-    `(do
-       (let [~!state (atom {})
-             ~!el (atom nil)
-             ~@(apply concat (fp/bindings compiled-elem))]
-                  
-         (letfn [(~notify! [~new-state ~updated-vars]
-                   (let [$old-el# @~!el
-                         $new-el# (fd/->node ~(fp/updated-form compiled-elem new-state updated-vars))]
-                     (when-not (= $old-el# $new-el#)
-                       (fd/swap-elems! $old-el# $new-el#))))
-                 
-                 (~handler [dep-sym#]
-                   (fn [~'_ ~'_ old-value# new-value#]
-                     (when (not= old-value# new-value#)
-                       (let [~new-state (swap! ~!state assoc dep-sym# new-value#)]
-                         (~notify! ~new-state #{dep-sym#})))))]
-
-           ~@(for [dep deps]
-               `(do
-                  (swap! ~!state assoc (quote ~dep) (deref ~dep))
-                  (add-watch ~dep ~(str (gensym "watch")) (~handler (quote ~dep)))))
-
-           (let [~state @~!state
-                 $initial-el# (fd/->node ~(fp/initial-form compiled-elem state))]
-             
-             (reset! ~!el $initial-el#)
-             $initial-el#))))))
+(defn render-elem [compiled-elem path]
+  `(flow.render/build-el {:deps [~@(when (satisfies? fp/CompiledIdentity compiled-elem)
+                                     (for [dep (set (concat (fp/hard-deps compiled-elem)
+                                                            (fp/soft-deps compiled-elem)))]
+                                       `{:dep-sym (quote ~dep)
+                                         :dep ~dep}))]
+                          :build-fn #(~@(fp/build-elem compiled-elem))}))
