a3bf042b489379dfbc375e92f96d388119b9bce6
James Henderson <james@jarohen.me.uk>
2014-08-03 13:51:18 +0100

    Removing more old code

--- src/flow/protocols.cljs
+++ src/flow/protocols.cljs
@@ -1,18 +1,4 @@
-(ns flow.protocols
-  (:require [flow.dom :as fd]))
-
-(defprotocol DynamicValue
-  (build [_ state])
-  (updated-value [_ new-state updated-vars]))
-
-(extend-protocol DynamicValue
-  nil
-  (build [_ _]
-    nil)
-
-  (updated-value [_ _ _]
-    nil))
-
+(ns flow.protocols)
 
 (defprotocol FlowElement
   (get-flow-id [_])
--- src/flow/render.clj
+++ src/flow/render.clj
@@ -7,9 +7,10 @@
 (defn render-elem [compiled-elem]
   `(do
      ~@(fp/declarations compiled-elem)
-     (flow.render/build-el {:deps [~@(when (satisfies? fp/CompiledIdentity compiled-elem)
-                                      (for [dep (set (concat (fp/hard-deps compiled-elem)
-                                                             (fp/soft-deps compiled-elem)))]
-                                        `{:dep-sym (quote ~dep)
-                                          :dep ~dep}))]
-                           :build-fn #(~@(fp/build-form compiled-elem))})))
+
+     (flow.render/build-el {:deps [~@(for [dep (set (concat (fp/hard-deps compiled-elem)
+                                                            (fp/soft-deps compiled-elem)))]
+                                       `{:dep-sym (quote ~dep)
+                                         :dep ~dep})]
+
+                            :build-fn #(~@(fp/build-form compiled-elem))})))
