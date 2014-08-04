13ed0d392fb8ec5aaa6d825d566d5245e203551e
James Henderson <james@jarohen.me.uk>
2014-08-04 22:55:48 +0100

    Going to alpha1 :)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/flow "0.2.0-SNAPSHOT"
+(defproject jarohen/flow "0.2.0-alpha1"
   :description "Lightweight library to help you write dynamic CLJS webapps"
 
   :url "https://github.com/james-henderson/flow"
