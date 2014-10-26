329d1fa57d053c6f65e252c6e803338ebc7fa8b7
James Henderson <james@jarohen.me.uk>
2014-10-26 22:46:35 +0000

    Adding support for 'case'

--- src/flow/compiler.clj
+++ src/flow/compiler.clj
@@ -1,10 +1,27 @@
 (ns flow.compiler
   (:require [flow.expand :refer [expand-macros]]))
 
+(defn resolve-fn [called-fn]
+  (let [resolved-fn (or (when (symbol? called-fn)
+                          (resolve called-fn))
+                        called-fn)]
+    (if (and (var? resolved-fn)
+             (contains? #{'clojure.core 'cljs.core} (symbol (str (:ns (meta resolved-fn))))))
+      (keyword "core" (str (:name (meta resolved-fn))))
+      resolved-fn)))
+
 (defn fn-call-type [[called-fn & args]]
-  (condp = (or (resolve called-fn) called-fn)
+  (condp = (resolve-fn called-fn)
     '<< :unwrap-lens
+
     'if :if
+    :core/case :case
+    :core/let :let
+    :core/for :for
+
+    'quote (throw (UnsupportedOperationException. "(quote ...) not supported in Flow's 'el' (yet?!)"))
+    'loop* (throw (UnsupportedOperationException. "loop/recur not supported in Flow's 'el'"))
+    
     :fn-call))
 
 (defn form-type [form {:keys [type]}]
@@ -48,6 +65,7 @@
 (require 'flow.forms.symbols)
 (require 'flow.forms.fn-calls)
 (require 'flow.forms.if)
+(require 'flow.forms.case)
 
 (defn compile-el [el-form macro-env]
   (-> el-form
