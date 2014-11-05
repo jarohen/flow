147b5611c9a2c405ff2b8db3b84a282c09ac9de4
James Henderson <james@jarohen.me.uk>
2014-11-05 15:21:19 +0000

    Bumping version and adding Changelog entry

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jarohen/flow "0.2.0-SNAPSHOT"
+(defproject jarohen/flow "0.2.0-beta4"
   :description "Lightweight library to help you write dynamic CLJS webapps"
 
   :url "https://github.com/james-henderson/flow"
